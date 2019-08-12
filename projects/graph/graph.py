cd4ee9885b3f0affea2dea726dbdb902fa622e04
nomadic4life <orbison.dennis@gmail.com>
2019-08-12 14:43:06 -0700

    bft and dft

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -27,20 +27,39 @@ class Graph:
             print("WARNING: vertex does not exist")
         else:
             self.vertices[v1].add(v2)
+            # self.vertices[v2].add(v1)
 
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        visited = set()
+        queue = Queue()
+        queue.enqueue(starting_vertex)
+        while queue.size() > 0:
+            vertex = queue.dequeue()
+            if vertex not in visited:
+                visited.add(vertex)
+                print(vertex)
+                for neighbor in self.vertices[vertex]:
+                    queue.enqueue(neighbor)
 
     def dft(self, starting_vertex):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        visited = set()
+        stack = Stack()
+        stack.push(starting_vertex)
+        while stack.size() > 0:
+            vertex = stack.pop()
+            if vertex not in visited:
+                visited.add(vertex)
+                print(vertex)
+                for neighbor in self.vertices[vertex]:
+                    stack.push(neighbor)
 
     def dft_recursive(self, starting_vertex):
         """
